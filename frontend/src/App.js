443604b1c5118469de1b88a776b7ee262d9780ec
Aditee Singh <singhaditee33@gmail.com>
2021-08-22 01:04:19 +0530

    cache cleared

---
+++ frontend/src/App.js
@@ -0,0 +1,165 @@
+import { useEffect, useState } from 'react';
+import ReactMapGL, { Marker , Popup } from 'react-map-gl';
+import { Room, Star } from '@material-ui/icons'
+import "./app.css";
+import axios from "axios";
+import {format} from 'timeago.js'
+import Register from './components/Register';
+import Login from './components/Login';
+
+function App() {
+
+  const myStorage = window.localStorage;
+  const [currentUser, setUser] = useState(myStorage.getItem("user"));
+  const [pins , setPins] = useState([]);
+  const [newPlace , setNewPlace] = useState(null);
+  const [title , setTitle] = useState(null);
+  const [desc , setDesc] = useState(null);
+  const [rating , setRating] = useState(0);
+  const [currentPlaceId , setCurrenPlaceId] = useState(null);
+  const [showRegister, setShowRegister] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: 20,
+    longitude: 78,
+    zoom: 4
+  });
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get("/pins");
+        setPins(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins();
+  }, []);
+
+  const handleMarkerClick = (id , lat , long) => {
+    setCurrenPlaceId(id)
+    setViewport({...viewport, latitude: lat, longitude: long})
+  }
+
+  const handleAddClick = (e) => {
+    const [longitude, latitude] = e.lngLat;
+    setNewPlace({
+      lat: latitude,
+      long: longitude,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPin = {
+      username: currentUser,
+      title,
+      desc,
+      rating,
+      lat: newPlace.lat,
+      long: newPlace.long,
+    };
+
+    try {
+      const res = await axios.post("/pins", newPin);
+      setPins([...pins, res.data]);
+      setNewPlace(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleLogout = () => {
+    myStorage.removeItem("user");
+    setUser(null);
+  }
+
+  return (
+    <div className="App">
+      <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+      onViewportChange={nextViewport => setViewport(nextViewport)}
+      mapStyle="mapbox://style/safak/cknndpyfq268f17p53nmpwira"
+      onDblClick={handleAddClick}
+      transitionDuration="200">
+        {pins.map((p) => (
+          <>
+          <Marker latitude={p.lat} 
+          longitude={p.long} 
+          offsetLeft={-viewport.zoom * 7} 
+          offsetTop={-viewport.zoom * 7}>
+          <Room style={{fontSize: viewport.zoom * 7, 
+            color: p.username === currentUser ? "tomato":"slateblue" , cursor:"pointer"}} 
+            onClick={() => handleMarkerClick(p._id , p.lat , p.long)}
+          />
+          </Marker>
+          {p._id === currentPlaceId && (
+            <Popup
+              latitude={p.lat}
+              longitude={p.long}
+              closeButton={true}
+              closeOnClick={false}
+              anchor="bottom" 
+              onClose={()=>setCurrenPlaceId(null)}>
+              <div className="card">
+                <label>Place</label>
+                <h4 className="place">{p.title}</h4>
+                <label>review</label>
+                <p className="description">{p.desc}</p>
+                <label>Rating</label>
+                <div className="stars">
+                  {Array(p.rating).fill(<Star className="star" />)}
+                </div>
+                <label>Information</label>
+                <span className="username">Created by <b className="name">{p.username}</b></span>
+                <span className="date">{format(p.createdAt)}</span>
+              </div>
+            </Popup> 
+          )}
+          </>
+        ))}
+        {newPlace && (
+          <Popup
+          latitude={newPlace.lat}
+          longitude={newPlace.long}
+          closeButton={true}
+          closeOnClick={false}
+          anchor="bottom" 
+          onClose={()=>setNewPlace(null)}>
+            <div>
+              <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input placeholder="Enter a title" onChange={(e)=>setTitle(e.target.value)}/>
+                <label>Review</label>
+                <textarea placeholder="Say something about this place!" onChange={(e)=>setDesc(e.target.value)}/>
+                <label>Rating</label>
+                <select onChange={(e)=>setRating(e.target.value)}>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </select>
+                <button className="submitButton" type="submit">Add Pin</button>
+              </form>
+            </div>
+        </Popup>
+        )}
+        {currentUser ? (<button className="button logout" onClick={handleLogout}>Log out</button>) : (
+          <div className="buttons">
+          <button className="button login" onClick={() => setShowLogin(true)}>Log in</button>
+          <button className="button register" onClick={() => setShowRegister(true)}>Register</button>
+        </div>
+        )}
+        {showRegister && <Register setShowRegister={setShowRegister}/>}
+        {showLogin&& <Login setShowLogin={setShowLogin} myStorage={myStorage} setUser={setUser}/>}
+    </ReactMapGL>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,48 @@
+import { Room, Cancel } from '@material-ui/icons'
+import { useState, useRef } from 'react';
+import "./login.css";
+import axios from "axios";
+
+export default function Login({setShowLogin , myStorage, setUser}){
+    const [error, setError] = useState(false);
+
+
+    const nameRef = useRef();
+    const passwordRef = useRef();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const user = {
+          username:  nameRef.current.value,
+          password: passwordRef.current.value,
+        };
+    
+        try {
+          const res = await axios.post("/users/login", user);
+          myStorage.setItem("user" , res.data.username)
+          setUser(res.data.username);
+          setShowLogin(false);
+          setError(false);
+        } catch (err) {
+          setError(true);
+        }
+      };
+
+    return (
+        <div className="container">
+            <div className="logo">
+                <Room />
+                Travel Buddy
+            </div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="username" ref={nameRef} />
+                <input type="password" placeholder="password" ref={passwordRef}/>
+                <button className="loginBtn">Login</button>
+                {error && (
+                    <span className="failure">Something wen wrong! Try again!</span>
+                )}
+            </form>
+            <Cancel className="loginCancel" onClick={() => setShowLogin(false)} />
+        </div>
+    )
+}
